c55661fa890ef07ceca89975fa08600ae4a776d9
keshav021 <keshavag007@gmail.com>
2019-01-29 12:43:48 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello Jarvis");
-		System.out.println("Hello New Pro");
+		System.out.println("Hello New Pro")
 	}
 }
